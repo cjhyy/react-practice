a0bc93b4894e8d5b02b095466727502e68bd9b15
maki maki <chenjunhong54321@163.com>
2020-03-13 23:21:03 +0800

    add

--- 初燕坊/birdhome/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    singleQuote: true,
-    semi: false,
-    printWidth: 120,
-    bracketSpacing: true,
-    arrowParens: 'avoid',
-    tabWidth: 4
-}
--- 初燕坊/birdhome/typings.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.pcss' {
-    interface IClassNames {
-        [className: string]: string
-    }
-    const classNames: IClassNames
-    export = classNames
-}
